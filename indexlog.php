5a164c63029c88daac3c9c888dc6897570969b06
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2019-09-25 01:11:32 +0100

    Working product

--- indexlog.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_SESSION['userid'])) {
-   echo '<p class="login-status">You are now logged in</p>';
-   }
-   else{
-    echo '<p class="login-status">You are now logged out</p>';
-   }
-?>
